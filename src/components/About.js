83acd42bdac9821a6f4011519ac3f709e078c97a
Jacob White <jacobewhitedev@gmail.com>
2020-07-12 05:30:36 -0500

    quote page update/about page update

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,20 @@ import React from "react";
 
 const About = () => {
   return (
-    <div id="about">
+    <div id="about-section">
       <img
         id="aboutPic"
-        src={require("../imgs/aboutPic.JPG")}
+        src={require("../imgs/aboutPic.jpg")}
         alt="Jacob White"
       />
+      <p>
+        <span className="first-letter">C</span>oming fresh out of high school
+        with my Associates Degree, I knew college was not the path for me. I
+        wanted to pursue a career path that was based on skills and hard work.
+        My brother-in-law introduced me to coding and since then, I have
+        completed the Treehouse TechDegree in Javascript, multiple Treehouse
+        Courses, and spend my spare time improving my skills as a programmer.
+      </p>
     </div>
   );
 };
--- src/components/Quote.js
+++ src/components/Quote.js
@@ -4,8 +4,23 @@ import React from "react";
 const Quote = () => {
   return (
     <div className="quote-section">
-      <h1 id="quote">A really great quote, dont you think?</h1>
-      <a href="#about" className="quote-section__arrow">
+      <h1 id="quote">Hi, I'm Jacob White.</h1>
+      <h2 id="sub-quote">A Full Stack Web Developer</h2>
+      <div className="link-icons">
+        <a target="_blank" href="https://github.com/jacoberick">
+          <i className="fab fa-github"></i>
+        </a>
+        <a target="_blank" href="https://twitter.com/jacobwhitedev">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/jacob-white-9356b719a"
+        >
+          <i className="fab fa-linkedin"></i>
+        </a>
+      </div>
+      <a href="#about-section" className="quote-section__arrow">
         <i className="arrow down" />
       </a>
     </div>
