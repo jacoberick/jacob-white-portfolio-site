62814c1b6483f4870abc8fa595f2ef1971cd5e89
Jacob White <jacobewhitedev@gmail.com>
2020-08-14 16:20:10 -0500

    Updates

---
+++ precache-manifest.c734f60dc5cfbca45601793ca98be366.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebda7c365c9b261538a465ca744a2262",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a257067a3d2701f2440",
+    "url": "/static/css/main.56808f6c.chunk.css"
+  },
+  {
+    "revision": "31826cc71694c5b97824",
+    "url": "/static/js/2.46908687.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.46908687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a257067a3d2701f2440",
+    "url": "/static/js/main.3c07ba65.chunk.js"
+  },
+  {
+    "revision": "680d77a408a6b4bddf40",
+    "url": "/static/js/runtime-main.1076d7c6.js"
+  },
+  {
+    "revision": "28486eb240956b1f92ea8d1695607621",
+    "url": "/static/media/LPF.28486eb2.png"
+  },
+  {
+    "revision": "77231130a942cbd6610bfc5f4e09247a",
+    "url": "/static/media/OOP.77231130.png"
+  },
+  {
+    "revision": "91da934f5ea2cbe43b6918aeaa5289b4",
+    "url": "/static/media/PAR.91da934f.png"
+  },
+  {
+    "revision": "bf87acf68f40dac214cb8c54fd237792",
+    "url": "/static/media/SQL.bf87acf6.png"
+  },
+  {
+    "revision": "13a1879a2f7195a4b2c58fe2697b07c7",
+    "url": "/static/media/aboutPic.13a1879a.jpg"
+  },
+  {
+    "revision": "81e330f07f558dd8f261c9abb11e0b21",
+    "url": "/static/media/fullStackApp.81e330f0.png"
+  },
+  {
+    "revision": "a8a8fdc7d4abd0a4336f5c111df41584",
+    "url": "/static/media/toDo.a8a8fdc7.png"
+  }
+]);
