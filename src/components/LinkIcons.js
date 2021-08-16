25fe54ca29f70635b049b4a4c8c5386df190b0e9
Jacob White <jacobewhitedev@gmail.com>
2021-08-16 01:30:33 -0500

    sass implementation and added codepen link

--- src/components/LinkIcons.js
+++ src/components/LinkIcons.js
@@ -1,30 +1,23 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTwitter,
-  faGithub,
-  faLinkedin,
-} from "@fortawesome/free-brands-svg-icons";
+import { faTwitter, faGithub, faCodepen, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const LinkIcons = () => {
   return (
     <div className="link-icons">
-      <a
-        rel="noopener noreferrer"
-        target="_blank"
-        href="https://github.com/jacoberick"
-      >
+      <a className="link-tags" rel="noopener noreferrer" target="_blank" href="https://github.com/jacoberick">
         <i className="fab fa-github"></i>
         <FontAwesomeIcon className="fab fa-github" icon={faGithub} />
       </a>
-      <a
-        rel="noopener noreferrer"
-        target="_blank"
-        href="https://twitter.com/jacobwhitedev"
-      >
+      <a className="link-tags" rel="noopener noreferrer" target="_blank" href="https://codepen.io/jacoberick">
+        <i className="fab fa-github"></i>
+        <FontAwesomeIcon className="fab fa-codepen" icon={faCodepen} />
+      </a>
+      <a className="link-tags" rel="noopener noreferrer" target="_blank" href="https://twitter.com/jacobwhitedev">
         <FontAwesomeIcon className="fab fa-twitter" icon={faTwitter} />
       </a>
       <a
+        className="link-tags"
         rel="noopener noreferrer"
         target="_blank"
         href="https://www.linkedin.com/in/jacob-white-9356b719a"
