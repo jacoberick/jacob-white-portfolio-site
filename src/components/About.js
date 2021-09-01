6e86b55d2f05ba1491082681ae2157a3567be1da
Jacob White <jacobewhitedev@gmail.com>
2021-09-01 17:02:23 -0500

    updated css, fixed footer icons, fixed drop letter, fixed fixed heights, added necessary padding

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,15 @@ import React from "react";
 const About = () => {
   return (
     <div id="aboutSection">
-      <img id="aboutPic" src={require("../imgs/aboutPic.JPG")} alt="Jacob White" />
-      <p>
-        <span className="first-letter">G</span>raduating from high school with my Associate degree, I was eager to
-        pursue a field of study that was self-motivated, flexible, and rooted in technology. My brother-in-law
-        introduced me to coding and it hit all the marks! I instantly began courses with Treehouse and obtained their
-        Full Stack Javascript Techdegree. Since then, I've been working on my own projects every single day.
-      </p>
+      <div id="aboutInner">
+        <img id="aboutPic" src={require("../imgs/aboutPic.JPG")} alt="Jacob White" />
+        <p>
+          <span className="first-letter">G</span>raduating from high school with my Associate degree, I was eager to
+          pursue a field of study that was self-motivated, flexible, and rooted in technology. My brother-in-law
+          introduced me to coding and it hit all the marks! I instantly began courses with Treehouse and obtained their
+          Full Stack Javascript Techdegree. Since then, I've been working on my own projects every single day.
+        </p>
+      </div>
     </div>
   );
 };
